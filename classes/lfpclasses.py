858b102d774035e989126711ac54a323216d3e9c
John Pearson <jmxpearson@gmail.com>
2015-12-22 17:40:32 -0500

    altered LFPset to use combination of censorings

--- classes/lfpclasses.py
+++ classes/lfpclasses.py
@@ -18,7 +18,7 @@ class LFPset(object):
         # wrap passed data in constructor in case it's a series
         # don't use setattr, since this will cause recursion
         self.__dict__['dataframe'] = pd.DataFrame(data)
-        self.meta = meta  # dict of metadata 
+        self.meta = meta  # dict of metadata
 
     def __getattr__(self, name):
         return getattr(self.dataframe, name)
@@ -72,7 +72,7 @@ class LFPset(object):
         return LFPset(newdf, newmeta)
 
     def instpwr(self):
-        # by padding up to next highest power of 2, we get a huge 
+        # by padding up to next highest power of 2, we get a huge
         # performance boost; truncate afterward
         Nstart = self.dataframe.shape[0]
         Nfft = 2 ** np.ceil(np.log2(Nstart))
@@ -83,7 +83,7 @@ class LFPset(object):
         return LFPset(newdf, newmeta)
 
     def instphase(self):
-        # by padding up to next highest power of 2, we get a huge 
+        # by padding up to next highest power of 2, we get a huge
         # performance boost; truncate afterward
         Nstart = self.dataframe.shape[0]
         Nfft = 2 ** np.ceil(np.log2(Nstart))
@@ -96,7 +96,7 @@ class LFPset(object):
 
     def smooth(self, winlen):
         wid = np.around(winlen * self.meta['sr'])
-        newdf = pd.rolling_mean(self.dataframe, wid, 
+        newdf = pd.rolling_mean(self.dataframe, wid,
             min_periods=1)
         newdf = newdf.apply(pd.Series.interpolate)
         newmeta = self.meta.copy()
@@ -104,12 +104,12 @@ class LFPset(object):
 
     def censor(self, maskarr=None, get_censor=None):
         """
-        Censor lfp, replacing with NaNs. Censoring has a default 
+        Censor lfp, replacing with NaNs. Censoring has a default
         behavior of removing railing artifacts separately for each
         column, but this behavior can be overridden by specifying either:
         1) maskarr, a Boolean numpy array suitable to serve as a mask
         for the data (i.e., True for censored values)
-        2) get_censor, a function that is passed self and should 
+        2) get_censor, a function that is passed self and should
         return a mask the same shape as self.values
         Option 1 takes precedence over option 2
         """
@@ -119,20 +119,22 @@ class LFPset(object):
         elif get_censor:
             m = get_censor(self)
         else:
-            m = np.apply_along_axis(cleaning.censor_railing, axis=0, 
+            mrail = np.apply_along_axis(cleaning.censor_railing, axis=0,
                 arr=self.dataframe.values)
-        
-        return LFPset(self.mask(m), self.meta.copy())
+            mburst = np.apply_along_axis(cleaning.censor_burst, axis=0,
+                arr=self.dataframe.values)
+
+        return LFPset(self.mask(mrail | mburst), self.meta.copy())
 
     def evtsplit(self, times, Tpre, Tpost, t0=0):
         # note: Tpre < 0 for times before the events in times
-        split_to_series = lambda x: core._splitseries(x, times, Tpre, Tpost, 
+        split_to_series = lambda x: core._splitseries(x, times, Tpre, Tpost,
             t0).unstack()
         return self.dataframe.apply(split_to_series)
 
     def avg_time_frequency(self, channel, times, Tpre, Tpost, method='wav', doplot=True, dbscale=True, **kwargs):
         """
-        Do a time frequency decomposition, averaging across chunks split at 
+        Do a time frequency decomposition, averaging across chunks split at
         times. **kwargs are passed on as parameters to the method call:
         method 'wav': w parameter
         method 'spec': window length (in s) and fraction overlap
@@ -146,7 +148,7 @@ class LFPset(object):
         else:
             callback = tf.spectrogram
 
-        avgtf = tf.avg_time_frequency(series, callback, times, Tpre, 
+        avgtf = tf.avg_time_frequency(series, callback, times, Tpre,
             Tpost, **kwargs)
 
         if doplot:
@@ -160,7 +162,7 @@ class LFPset(object):
         """
         Do a contrast analysis for two sets of events. times is an iterable
         containing times for each. Returned value is a ratio of time-frequency
-        power in the first set vs. the second. **kwargs are passed on as 
+        power in the first set vs. the second. **kwargs are passed on as
         parameters to the method call:
         method 'bandwidth': bandwidth parameter
         method 'spec': window length (in s) and fraction overlap
@@ -171,9 +173,9 @@ class LFPset(object):
         else:
             callback = tf.spectrogram
 
-        tf0 = tf.avg_time_frequency(series, callback, times[0], Tpre, 
+        tf0 = tf.avg_time_frequency(series, callback, times[0], Tpre,
             Tpost, **kwargs)
-        tf1 = tf.avg_time_frequency(series, callback, times[1], Tpre, 
+        tf1 = tf.avg_time_frequency(series, callback, times[1], Tpre,
             Tpost, **kwargs)
 
         if 'clim' in kwargs:
@@ -183,22 +185,22 @@ class LFPset(object):
             color_lims = None
 
         if doplot:
-            fig = tf.plot_time_frequency(tf0 / tf1, dbscale=dbscale, clim=color_lims, **kwargs) 
+            fig = tf.plot_time_frequency(tf0 / tf1, dbscale=dbscale, clim=color_lims, **kwargs)
         else:
             fig = None
 
         return tf0 / tf1, fig
 
-    def significant_time_frequency(self, channel, times, Tpre, Tpost, thresh, niter=1000, pval=0.05, method='wav', doplot=True, **kwargs): 
+    def significant_time_frequency(self, channel, times, Tpre, Tpost, thresh, niter=1000, pval=0.05, method='wav', doplot=True, **kwargs):
         """
-        Given a data series determined by channel, a two-element iterable, 
-        times, containing times for a pair of events, pre and post-event 
-        windows to grab, a scalar threshold (for symmetric 
-        thresholding) or a pair of thresholds (lo, hi) at which to 
+        Given a data series determined by channel, a two-element iterable,
+        times, containing times for a pair of events, pre and post-event
+        windows to grab, a scalar threshold (for symmetric
+        thresholding) or a pair of thresholds (lo, hi) at which to
         cut clusters, a number of bootstrap iterations, and a p-value for
         statistical significance,
         return a time-frequency dataframe (suitable for plotting) containing
-        the statistically significant clusters in the contrast between the 
+        the statistically significant clusters in the contrast between the
         two conditions (times[0] - times[1]).
         """
         series = self.dataframe[channel]
@@ -218,4 +220,4 @@ class LFPset(object):
         else:
             fig = None
 
-        return dfcontrast, fig 
+        return dfcontrast, fig
